28a079a3da740670b32415db7120d6cf09020a93
Rafael Almeida <148403445+rrafaum@users.noreply.github.com>
2025-11-22 02:09:58 -0300

    next.config.ts configured

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org', // Essencial para os cartazes dos filmes (TMDB)
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com', // Para fotos de perfil do GitHub
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // Caso use fotos do Google no futuro
+      },
+    ],
+  },
 };
 
 export default nextConfig;
